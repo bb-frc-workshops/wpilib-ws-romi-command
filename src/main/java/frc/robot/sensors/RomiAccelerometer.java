afe9bff2f1489bcbda96d3cb8e51b848f2cada51
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2020-12-06 15:07:16 -0500

    IMU example

---
+++ src/main/java/frc/robot/sensors/RomiAccelerometer.java
@@ -0,0 +1,32 @@
+package frc.robot.sensors;
+
+public class RomiAccelerometer {
+    private SimDevice m_simDevice;
+    private SimDouble m_simAccelX;
+    private SimDouble m_simAccelY;
+    private SimDouble m_simAccelZ;
+    private SimDouble m_simAccelSensitivity;
+
+    public SimAccelerometer(int sensitivity) {
+        m_simDevice = SimDevice.create("Accel:BuiltInAccelerometer");
+        if (m_simDevice != null) {
+            System.out.println("Creating SimValues");
+            m_simAccelX = m_simDevice.createDouble("x", false, 0.0);
+            m_simAccelY = m_simDevice.createDouble("y", false, 0.0);
+            m_simAccelZ = m_simDevice.createDouble("z", false, 0.0);
+            m_simAccelSensitivity = m_simDevice.createDouble("range", true, sensitivity);
+        }
+    }
+
+    public double getX() {
+        return m_simAccelX.get();
+    }
+
+    public double getY() {
+        return m_simAccelY.get();
+    }
+
+    public double getZ() {
+        return m_simAccelZ.get();
+    }
+}
---
+++ src/main/java/frc/robot/sensors/RomiGyro.java
@@ -0,0 +1,48 @@
+package frc.robot.sensors;
+
+public class RomiGyro {
+    private SimDevice m_simDevice;
+    private SimDouble m_simRateX;
+    private SimDouble m_simRateY;
+    private SimDouble m_simRateZ;
+    private SimDouble m_simAngleX;
+    private SimDouble m_simAngleY;
+    private SimDouble m_simAngleZ;
+
+    public SimGyro() {
+        m_simDevice = SimDevice.create("Gyro:RomiGyro");
+        if (m_simDevice != null) {
+            m_simRateX = m_simDevice.createDouble("rateX", false, 0.0);
+            m_simRateY = m_simDevice.createDouble("rateY", false, 0.0);
+            m_simRateZ = m_simDevice.createDouble("rateZ", false, 0.0);
+
+            m_simAngleX = m_simDevice.createDouble("angleX", false, 0.0);
+            m_simAngleY = m_simDevice.createDouble("angleY", false, 0.0);
+            m_simAngleZ = m_simDevice.createDouble("angleZ", false, 0.0);
+        }
+    }
+
+    double getRateX() {
+        return m_simRateX.get();
+    }
+
+    double getRateY() {
+        return m_simRateY.get();
+    }
+
+    double getRateZ() {
+        return m_simRateZ.get();
+    }
+
+    double getAngleX() {
+        return m_simAngleX.get();
+    }
+
+    double getAngleY() {
+        return m_simAngleY.get();
+    }
+
+    double getAngleZ() {
+        return m_simAngleZ.get();
+    }
+}
